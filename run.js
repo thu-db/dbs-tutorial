00bb0be891b97fde73b56c7cca54fc81c47b724f
rcy <rcycyzh@163.com>
2021-08-25 15:05:07 +0800

    Fix bugs

--- run.js
+++ run.js
@@ -14,9 +14,11 @@ const current = new Date().toLocaleString("zh-cn", {hour12: false, timeZone: "As
 
 let fileData = fs.readFileSync('docs/SUMMARY.md', {encoding: "utf-8"});
 const iterator = fileData.matchAll( /\(([^)]*)\)/g );
+const prefix = "docs/"
 for (let it = iterator.next(); !it.done; it = iterator.next()) {
-    const filename = 'docs/' + it.value[1];
-    const targetData = fs.readFileSync(filename, {encoding: "utf-8"});
+    const filename = it.value[1];
+    const path = prefix + filename;
+    const targetData = fs.readFileSync(path, {encoding: "utf-8"});
     const md5 = MD5(targetData).toString();
     if (md5 != record[filename]?.md5) {
         console.log("update file " + filename);
@@ -30,7 +32,11 @@ for (let it = iterator.next(); !it.done; it = iterator.next()) {
 fs.writeFileSync('timestamp/data.json', JSON.stringify(record), {encoding: "utf-8"});
 
 
-rawdata = fs.readFileSync('book.json', {encoding: "utf-8"});
-const book = JSON.parse(rawdata);
-book.timestamp = record;
-fs.writeFileSync('book.json', JSON.stringify(book), {encoding: "utf-8"});
+rawdata = fs.readFileSync('docs/footer.md', {encoding: "utf-8"});
+
+jsonData = JSON.stringify(record)
+// We should prevent "}}"
+jsonData = jsonData.substring(0, jsonData.length - 1) + ' }'
+rawdata = `{% set timestamp = ${jsonData} %}\n` + rawdata
+
+fs.writeFileSync('docs/footer.md', rawdata, {encoding: "utf-8"});
